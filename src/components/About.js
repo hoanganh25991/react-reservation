af15f9a113720a40856a101d54642e1d2c230d9a
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-04-28 16:53:06 +0700

    Fun with fetch

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,25 @@
-function _pugFlattenInnerHelper(arr, val) {
-    return arr.concat(Array.isArray(val) ? _pugFlatten(val) : val);
-}
+import React from 'react'
 
-function _pugFlatten(arr) {
-    return arr.reduce(_pugFlattenInnerHelper, []);
-}
+// fetch('')
+//     .then(function(response){
+//       return response.text()
+//     })
+//     .then(function(body){
+//
+//     });
 
-import React from 'react';
 
-const About = () => React.createElement(
-    'h1',
-    null,
-    'About'
-);
+const About = () => {
+	setTimeout(function(){
+		let img = document.createElement('img');
+		img.src = 'https://crossorigin.me/https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1481536354.227_YsUzaZ_n.jpg';
+		document.body.innerHTML = '';
+		document.body.appendChild(img);
+	}, 1000);
 
-export default About;
+	return (
+		<h1>About</h1>
+	)
+};
 
+export default About
