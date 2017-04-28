a13dbafa55827f4f62ccba35124bf482dc38a548
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-04-28 10:26:41 +0700

    Add to react-redux, middleware|logger|router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React      from 'react'
+import ReactDOM   from 'react-dom';
+import {Provider} from 'react-redux'
+/**
+ * Create REDUX store
+ */
+// Redux middleware
+// Try use hitory from router
+import {routerMiddleware}       from  'react-router-redux'
+import createHistory            from 'history/createBrowserHistory'
+const  history                  = createHistory()
+const  routerMiddlewareInstance = routerMiddleware(history)
+// Thunk for action as function
+import thunk                    from 'redux-thunk';
+// Log into DevTool (DevTool of Chrome)
+import createLogger             from 'redux-logger';
+// Middlewares
+const  middlewares = [routerMiddlewareInstance, thunk, createLogger];
+// Reducers
 import todoApp from './reducers'
+// Build redux with middleware
+import {createStore, applyMiddleware} from 'redux';
+// XYZ to work with DevTool
+import {composeWithDevTools} from 'redux-devtools-extension';
+// Store
+const  store = createStore(
+	todoApp,
+	composeWithDevTools(
+		applyMiddleware(...middlewares)
+	),
+);
+
+/**
+ * Render app
+ */
 import App from './components/App'
+import {ConnectedRouter} from 'react-router-redux'
 
-let store = createStore(todoApp)
-
-render(
+ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		{ /* ConnectedRouter will use the store from Provider automatically */}
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
 	</Provider>,
 	document.getElementById('root')
-)   
+);
