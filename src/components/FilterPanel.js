efa4afda383d89cc3bf34b31a0099912fda5d677
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-08 09:23:02 +0700

    show|hide on input pick a date base on state

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -2,36 +2,50 @@ import React from 'react'
 
 class FilterPanel extends React.Component {
 	render(){
+		// Get state
+		let {isVisibleInputPickADate} = this.props;
 		// Get actions
 		let {toggleInputPickADate} = this.props;
 
 
 		return(
-			<div className="flex-row bg-dark-blue pad30">
-				<div>
-					<h3 className="text-whit pad-large filter-selected">TODAY</h3>
+			<div>
+				<div className="flex-row bg-dark-blue pad30">
+					<div>
+						<h3 className="text-whit pad-large filter-selected">TODAY</h3>
+					</div>
+					<div>
+						<h3 className="text-whit pad-large">TOMORROW</h3>
+					</div>
+					<div>
+						<h3 className="text-whit pad-large">NEXT 3 DAYS</h3>
+					</div>
+					<div>
+						<h3 className="text-whit pad-large">NEXT 7 DAYS</h3>
+					</div>
+					<div>
+						<h3 className="text-whit pad-large">NEXT 30 DAYS</h3>
+					</div>
+					<div>
+						<h3 className="text-whit pad-large"
+						    onClick={() => toggleInputPickADate()}
+						>PICK A DATE</h3>
+					</div>
+					<div className="flex1"></div>
+					<div>
+						<h3 className="text-whit pad-large bg-yell">FILTER</h3>
+					</div>
 				</div>
-				<div>
-					<h3 className="text-whit pad-large">TOMORROW</h3>
-				</div>
-				<div>
-					<h3 className="text-whit pad-large">NEXT 3 DAYS</h3>
-				</div>
-				<div>
-					<h3 className="text-whit pad-large">NEXT 7 DAYS</h3>
-				</div>
-				<div>
-					<h3 className="text-whit pad-large">NEXT 30 DAYS</h3>
-				</div>
-				<div>
-					<h3 className="text-whit pad-large"
-						onClick={() => toggleInputPickADate()}
-					>PICK A DATE</h3>
-				</div>
-				<div className="flex1"></div>
-				<div>
-					<h3 className="text-whit pad-large bg-yell">FILTER</h3>
-				</div>
+				{
+					isVisibleInputPickADate ? (
+						<div className="flex-row">
+							<div className="flex1"></div>
+							<div>
+								<input type="date" />
+							</div>
+						</div>
+					) : null
+				}
 			</div>
 		)
 	}
@@ -47,7 +61,7 @@ import {connect} from 'react-redux'
 
 import {actionToggleInputPickADate} from '../actions'
 
-const mapStateToProps  = ({reservations, filters}) => ({reservations: filters.reduce((carry, filter) => (carry.filter(filter)), reservations)})
+const mapStateToProps  = ({toggleInputPickADate}) => ({isVisibleInputPickADate: toggleInputPickADate})
 
 const mapActionToProps = (dispatch) => {
 	return {
